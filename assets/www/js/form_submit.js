edb9219a23262261de046592cf9c9cc06cc967ab
Louise Fox <louise.s.fox@gmail.com>
2012-11-16 18:03:16 -0800

    added tracking, fixed events, added user reg

--- assets/www/js/form_submit.js
+++ assets/www/js/form_submit.js
@@ -1,20 +1,13 @@
 $('#login_submit').live('click', function(){
 	 
 	var url = "http://10.0.0.13:5000/m_authenticate";
-
 	var email_val=document.getElementById('email').value;
 	console.log(email_val);
-
 	var password_val=document.getElementById('password').value;
 	console.log(password_val);
-	
 	var data = {email: email_val, password: password_val};
 	console.log(data);
 
-
-	// $.getJSON(url,data,function(data){;
-	// console.log(data)};
-		
 	$.ajax({
 
 		type:'POST',
@@ -24,16 +17,47 @@ $('#login_submit').live('click', function(){
 			console.log(data);
 			var str_data=JSON.stringify(data);
 			console.log(str_data);
-
 			alert('You have successfully logged in to Dog Log. Get loggin');
 		},
 		error: function(data){
 			console.log(data);
-			
 			alert('There was an error with your email or password. Please try logging in again.');
 		}
 	});
+	return false;
+});
 
+$('#new_user').live('click', function(){
+	 
+	var url = "http://10.0.0.13:5000/m_save_user";
+	var first_name_val=document.getElementById('first_name').value;
+	var last_name_val=document.getElementById('last_name').value;
+	var company_name_val=document.getElementById('company_name').value;
+	var phone_val=document.getElementById('phone').value;
+	var email_val=document.getElementById('new_user_email').value;
+	console.log(email_val);
+	var password_val=document.getElementById('new_user_password').value;
+	console.log(password_val);
+	var data = {first_name: first_name_val, last_name: last_name_val, company_name: company_name_val,
+				phone: phone_val, email: email_val, password: password_val};
+	console.log(data);
+
+	$.ajax({
+
+		type:'POST',
+		data: data,
+		url: url,
+		success: function(data){
+			console.log(data);
+			var str_data=JSON.stringify(data);
+			console.log(str_data);
+		
+		},
+		error: function(data){
+			console.log(data);
+			alert('There was an error reg. you. Please try again.');
+		}
+	});
 	return false;
 });
 
--- assets/www/js/tracker.js
+++ assets/www/js/tracker.js
@@ -1,66 +1,84 @@
 
-var tracking_data = [] /// array containing cor. objects
-
+var tracking_data = []; 
+var event_data=[];/// array containing cor. objects
+var last_values=[];
+var watchId=null;
+var map=null;
+var condition=true;
+var pretty=null;
+var seconds = null;
+var ticker = null;
 function onSuccess(position){
 		
   var myLat = position.coords.latitude;
   var myLong = position.coords.longitude;
+  console.log(myLat,myLong);
   var myLatLng = new google.maps.LatLng(myLat, myLong);
-  var mapOptions = {
-    zoom: 15,
-    center: myLatLng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  
-  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+  map.setCenter(myLatLng)
+  tracking_data.push(myLatLng);
+  last_values=[myLat,myLong];
 
-  tracking_data.push(position);
+   var trackPath = new google.maps.Polyline({
+      path: tracking_data,
+      strokeColor: "#FF0000",
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
 
-  $("#poop").live('click',function(){
-    console.log('here');
-  var poop_image = '../www/img/poop.png';
+    // Apply the line to the map
+    trackPath.setMap(map);
+};
 
-    var poop_location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    var poop_marker = new google.maps.Marker({
+$("#poop").live('click',function(){
+  console.log('here');
+  var poop_image = '../www/css/img/poop.png';
+  var poop_location = new google.maps.LatLng(last_values[0], last_values[1]);
+  console.log(last_values[0],last_values[1])
+  var poop_marker = new google.maps.Marker({
     position: poop_location,
     map: map,
     icon: poop_image
+  });
+
+  event_data.push(poop_marker);
+ 
 });
-   console.log(poop_location);  
+
+
+$("#friend").live('click',function(){
+
+  var friend_image = '../www/css/img/dog_happy.gif';
+  var friend_location = new google.maps.LatLng(last_values[0], last_values[1]);
+  var friend_marker = new google.maps.Marker({
+    position: friend_location,
+    map: map,
+    icon: friend_image
 });
-};
+  event_data.push(friend_marker);
+});
+$("#frenemy").live('click',function(){
 
-// $("#friend").live('click',function(){
+  var frenemy_image = '../www/css/img/dog_sad.gif';
+  var frenemy_location = new google.maps.LatLng(last_values[0], last_values[1]);
+  var frenemy_marker = new google.maps.Marker({
+    position: frenemy_location,
+    map: map,
+    icon: frenemy_image
+});
+  event_data.push(frenemy_marker);
+});
 
-//   var friend_image = '../www/img/dog_happy.gif';
-//   var friend_location = new google.maps.LatLng(-34.397, 150.644);
-//   var friendmarker = new google.maps.Marker({
-//     position: friend_location,
-//     map: map,
-//     icon: friend_image
-// });
-// });
-// $("#frenemy").live('click',function(){
+$("#pee").live('click',function(){
 
-//   var frenemy_image = '../www/img/dog_sad.gif';
-//   var frenemy_location = new google.maps.LatLng(-34.397, 150.644);
-//   var frenemymarker = new google.maps.Marker({
-//     position: frenemy_location,
-//     map: map,
-//     icon: frenemy_image
-// });
-// });
-
-// $("#pee").live('click',function(){
-
-//   var pee_image = '../www/img/pee.gif';
-//   var pee_location = new google.maps.LatLng(-34.397, 150.644);
-//   var peemarker = new google.maps.Marker({
-//     position: pee_location,
-//     map: map,
-//     icon: pee_image
-// });
-// });
+  var pee_image = '../www/css/img/pee.gif';
+  var pee_location = new google.maps.LatLng(last_values[0], last_values[1]);
+  var pee_marker = new google.maps.Marker({
+    position: pee_location,
+    map: map,
+    icon: pee_image
+});
+  event_data.push(pee_marker);
+});
 
 function onError(error){
 
@@ -68,16 +86,57 @@ function onError(error){
 }
 
 $("#start_logging").live('click', function(){
+  var start = Date.now();
+	watchId = navigator.geolocation.watchPosition(onSuccess, onError,{frequency: 15000, enableHighAccuracy: true});
+  var default_center = new google.maps.LatLng(37.37,121.92);
+  pretty=null;
+  condition=true;
+  var mapOptions = {
+    zoom: 15,
+    center: default_center,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
 
-	var watchId = navigator.geolocation.watchPosition(onSuccess, onError,{frequency: 15000});
-  
+  };
+  startTimer(true);
+  map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 });
-
+  
 $("#end_logging").live('click', function(){
 
   navigator.geolocation.clearWatch(watchId);
   var track_data_str=JSON.stringify(tracking_data);
+  // var event_data_str=JSON.stringify(event_data);
+  tracking_data=[];
+  event_data=[];
+  condition=false;
   console.log(track_data_str);
+  // console.log(event_data_str);
   });
 
+function tick( )
+{
+  if (condition==true)
+  {   
+    ++seconds;
+    var secs = seconds;
+    var hrs = Math.floor( secs / 3600 );
+    secs %= 3600;
+    var mns = Math.floor( secs / 60 );
+    secs %= 60;
+    pretty = ( hrs < 10 ? "0" : "" ) + hrs
+               + ":" + ( mns < 10 ? "0" : "" ) + mns
+               + ":" + ( secs < 10 ? "0" : "" ) + secs;
+    document.getElementById("ELAPSED").innerHTML = pretty;
+  }
+  else{
+    document.getElementById("ELAPSED").innerHTML = pretty;
+}
+}
+
+function startTimer(condition)
+{
+    seconds = -1;
+    ticker = setInterval("tick( )", 1000);
+    tick(condition);
+}
 
